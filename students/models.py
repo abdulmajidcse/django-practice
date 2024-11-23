146f25d3d0f77729900b0e0a715301530fc26557
Abdul Majid <abdulmajidcse@gmail.com>
2024-11-23 23:48:23 +0600

    student and studentdetail model.

--- students/models.py
+++ students/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=191)
+    email = models.CharField(max_length=191)
+    mobile = models.CharField(max_length=20)
+    roll = models.IntegerField()
+    created_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class StudentDetail(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    father_name = models.CharField(max_length=191)
+    mother_name = models.CharField(max_length=191)
+    address = models.TextField(null=True)
+    nationality = models.CharField(max_length=191)
+    birth_date = models.DateField()
+    birth_certificate = models.IntegerField()
+    created_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True)
+    
+    def __str__(self):
+        return self.father_name
