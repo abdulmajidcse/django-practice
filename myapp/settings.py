c7650bf7b0ffcb93bf91ff5a424741f84d09eb2d
Abdul Majid <abdulmajidcse@gmail.com>
2024-11-23 23:09:35 +0600

    setup postgresql db and updated timezone.

--- myapp/settings.py
+++ myapp/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_practice',
+        'HOST': 'localhost',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
     }
 }
 
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Dhaka'
 
 USE_I18N = True
 
